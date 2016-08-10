39e02d133785828389d077b1a50e8b5b8c1c23a9
Bharath <bharath.sekar@ge.com>
2016-08-09 17:24:17 -0700

    Adding ResourceMigrationManager unit tests.
    Signed-off-by: Bhuvana <bhuvaneswari.ramkumar@ge.com>

--- service/src/main/java/com/ge/predix/acs/privilege/management/dao/ResourceMigrationManager.java
+++ service/src/main/java/com/ge/predix/acs/privilege/management/dao/ResourceMigrationManager.java
@@ -15,13 +15,29 @@ public final class ResourceMigrationManager {
     @Qualifier("resourceRepository")
     private ResourceRepository from;
 
+    @Autowired
+    @Qualifier("resourceHierarchicalRepository")
+    private GraphResourceRepository to;
+
+    public ResourceMigrationManager(final ResourceRepository from, final GraphResourceRepository to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public ResourceMigrationManager() {
+
+    }
+
     @PostConstruct
-    public int doMigration() {
+    public void doMigration() {
+        // TODO: Return value should reflect status of migration?
 
         System.out.println("doMigration called");
         List<ResourceEntity> allResources = from.findAll();
-        System.out.println("Resources obtained - All: " + allResources.size());
-        return allResources.size();
+        System.out.println("Should be 2: " + allResources.size());
+        to.save(allResources);
+        System.out.println("To-REPO Should be 2: " + to.findAll().size());
+        return;
         // PageRequest testPage = new PageRequest(0, 3);
         // List<ResourceEntity> listOfResources = from.findAll(testPage).getContent();
         // List<ResourceEntity> listOfResources = new ArrayList<ResourceEntity>();
