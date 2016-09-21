6e853738b53c259c775727b00385b77faa4a7e32
Matias Altman <matias.altman@ge.com>
2016-09-21 10:28:12 -0700

    de6793: Work around for delay in update of nurego's state  - Increased delay to 10 seconds  - Changed System.out calls to logger calls  - Added waitForSteadyStateEntitlementUsage to busy wait until meter has completely updated its state before test  - added isMeterUpdated and isUsageSteady to simplify check if meter was changed  - Made modifications to get the file in line with the checkstyle

--- acs-integration-tests/src/test/java/com/ge/predix/integration/test/ACSMeteringIT.java
+++ acs-integration-tests/src/test/java/com/ge/predix/integration/test/ACSMeteringIT.java
@@ -1,27 +1,28 @@
 /*******************************************************************************
- * Copyright 2016 General Electric Company. 
+ * Copyright 2016 General Electric Company.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
 package com.ge.predix.integration.test;
 
 import static com.ge.predix.integration.test.SubjectResourceFixture.MARISSA_V1;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -36,8 +37,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.ge.predix.acs.rest.PolicyEvaluationRequestV1;
 import com.ge.predix.acs.rest.PolicyEvaluationResult;
 import com.ge.predix.acs.rest.Zone;
@@ -53,6 +52,7 @@ import com.nurego.model.Subscription;
 @ContextConfiguration("classpath:integration-test-spring-context.xml")
 @Test
 public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ACSMeteringIT.class);
 
     @Autowired
     private ACSRestTemplateFactory acsRestTemplateFactory;
@@ -75,8 +75,13 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
     @Value("${NUREGO_API_KEY}")
     private String nuregoApiKey;
 
+    @Value("${STEADY_STATE_SLEEP_MS:10000}")
+    private int steadyStateSleepMS;
+
     private static final String POLICY_UPDATE_FEATURE_ID = "policyset_update";
     private static final String POLICY_EVAL_FEATURE_ID = "policy_eval";
+    private static final int MAX_ITERATIONS = 10;
+    private static final int NUREGO_UPDATE_SLEEP_MS = 1000;
     private Subscription subscription;
 
     private String zoneId;
@@ -89,7 +94,7 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
     private ZacTestUtil zacTestUtil;
 
     @BeforeClass
-    public void setup() throws JsonParseException, JsonMappingException, IOException {
+    public void setup() throws Exception {
         this.zacTestUtil.assumeZacServerAvailable();
 
         this.createPrimaryTestZone = this.zoneHelper.createPrimaryTestZone();
@@ -98,9 +103,12 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
         Nurego.setApiBase(this.nuregoApiBase);
         Nurego.apiKey = this.nuregoApiKey;
 
-        // createNuregoSubscription(this.createPrimaryTestZone.getSubdomain());
+        //Busy wait for the meter to achieve steady state for POLICY_UPDATE and POLICY_EVAL
+        waitForSteadyStateEntitlementUsage(POLICY_UPDATE_FEATURE_ID, this.zoneId);
+        waitForSteadyStateEntitlementUsage(POLICY_EVAL_FEATURE_ID, this.zoneId);
     }
 
+    @Test
     public void testACSMetering() throws Exception {
         String testPolicyName = null;
         PolicyEvaluationRequestV1 policyEvaluationRequest = this.policyHelper
@@ -110,8 +118,8 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
             Double beforePolicyUpdateMeterCount = getEntitlementUsageByFeatureId(POLICY_UPDATE_FEATURE_ID, this.zoneId);
             Double beforePolicyEvalMeterCount = getEntitlementUsageByFeatureId(POLICY_EVAL_FEATURE_ID, this.zoneId);
 
-            System.out.println("POLICY UPDATE USAGE BEFORE:" + beforePolicyUpdateMeterCount);
-            System.out.println("POLICY EVAL USAGE BEFORE:" + beforePolicyEvalMeterCount);
+            LOGGER.info("POLICY UPDATE USAGE BEFORE:" + beforePolicyUpdateMeterCount);
+            LOGGER.info("POLICY EVAL USAGE BEFORE:" + beforePolicyEvalMeterCount);
 
             OAuth2RestTemplate acsRestTemplate = this.acsRestTemplateFactory.getACSTemplateWithPolicyScope();
             String policyFile = "src/test/resources/single-site-based-policy-set.json";
@@ -123,18 +131,30 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
 
             Assert.assertEquals(evalResponse.getStatusCode(), HttpStatus.OK);
 
-            //Nurego server seems to have a lag before the counts are updated
-            Thread.sleep(3000);
+            // Nurego server seems to have a lag before the counts are updated
+            // Wait for Nurego to be updated before proceeding with assertion
+            Double afterPolicyUpdateMeterCount = 0.0;
+            Double afterPolicyEvalMeterCount = 0.0;
+            Double updateMeterCount = 0.0;
+            Double evalMeterCount = 0.0;
+            for (int i = 0; i < MAX_ITERATIONS; i++) {
+                Thread.sleep(NUREGO_UPDATE_SLEEP_MS);
+                afterPolicyUpdateMeterCount = getEntitlementUsageByFeatureId(POLICY_UPDATE_FEATURE_ID, this.zoneId);
+                afterPolicyEvalMeterCount = getEntitlementUsageByFeatureId(POLICY_EVAL_FEATURE_ID, this.zoneId);
 
-            Double afterPolicyUpdateMeterCount = getEntitlementUsageByFeatureId(POLICY_UPDATE_FEATURE_ID, this.zoneId);
-            Double afterPolicyEvalMeterCount = getEntitlementUsageByFeatureId(POLICY_EVAL_FEATURE_ID, this.zoneId);
-
-            System.out.println("POLICY UPDATE USAGE AFTER:" + afterPolicyUpdateMeterCount);
-            System.out.println("POLICY EVAL USAGE AFTER:" + afterPolicyEvalMeterCount);
+                if (isMeterUpdated(beforePolicyUpdateMeterCount, afterPolicyUpdateMeterCount)
+                        && isMeterUpdated(beforePolicyEvalMeterCount, afterPolicyEvalMeterCount)) {
+                    LOGGER.debug("POLICY UPDATE USAGE AFTER: " + afterPolicyUpdateMeterCount);
+                    LOGGER.debug("POLICY EVAL USAGE AFTER: " + afterPolicyEvalMeterCount);
+                    break;
+                }
+            }
 
             // Assert metering counts incremented by 1
-            Assert.assertEquals(afterPolicyUpdateMeterCount - beforePolicyUpdateMeterCount, 1.0);
-            Assert.assertEquals(afterPolicyEvalMeterCount - beforePolicyEvalMeterCount, 1.0);
+            updateMeterCount = afterPolicyUpdateMeterCount - beforePolicyUpdateMeterCount;
+            evalMeterCount = afterPolicyEvalMeterCount - beforePolicyEvalMeterCount;
+            Assert.assertEquals(updateMeterCount, 1.0);
+            Assert.assertEquals(evalMeterCount, 1.0);
         } finally {
             if (testPolicyName != null) {
                 this.policyHelper.deletePolicySet(testPolicyName);
@@ -174,6 +194,38 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
         return entitlement.getCurrentUsedAmount();
     }
 
+    private boolean waitForSteadyStateEntitlementUsage(final String featureId, final String subscriptionId)
+            throws Exception {
+        double usageBeforeWait = 0.0;
+        double usageAfterWait = 0.0;
+        int iteration;
+
+        for (iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
+            usageBeforeWait = getEntitlementUsageByFeatureId(featureId, subscriptionId);
+
+            Thread.sleep(steadyStateSleepMS);
+
+            usageAfterWait = getEntitlementUsageByFeatureId(featureId, subscriptionId);
+
+            if (isUsageSteady(usageBeforeWait, usageAfterWait)) {
+                String msg = String.format("Meter has reached a steady state of '%f' for '%s' with subscription '%s'",
+                        usageAfterWait, POLICY_UPDATE_FEATURE_ID, this.planId);
+                LOGGER.info(msg);
+                break;
+            }
+        }
+
+        return isUsageSteady(usageBeforeWait, usageAfterWait);
+    }
+
+    private boolean isMeterUpdated(final double readBeforeSleep, final double readAfterSleep) {
+        return !isUsageSteady(readBeforeSleep, readAfterSleep);
+    }
+
+    private boolean isUsageSteady(final double readBeforeSleep, final double readAfterSleep) {
+        return readBeforeSleep == readAfterSleep;
+    }
+
     @AfterClass
     public void cleanUp() {
         this.zoneHelper.deleteZone(this.createPrimaryTestZone.getSubdomain());
@@ -184,10 +236,10 @@ public class ACSMeteringIT extends AbstractTestNGSpringContextTests {
     // difference between usage amount would be one. If you want to create a new subscription please uncomment
     // the following code.
     @SuppressWarnings("unused")
-    private void createNuregoSubscription(String zoneId) {
+    private void createNuregoSubscription(final String subscriptionId) {
         Map<String, Object> params = new HashMap<>();
         params.put("plan_id", this.planId);
-        params.put("external_subscription_id", zoneId);
+        params.put("external_subscription_id", subscriptionId);
         params.put("provider", "cloud-foundry");
         try {
             this.subscription = Subscription.create(this.organizationId, params);
