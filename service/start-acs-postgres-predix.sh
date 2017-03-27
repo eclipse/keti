ad1270c0c42afb678be14992107f2a1b6dd5301c
Anubhav <anubhav.saggi@ge.com>
2017-03-27 14:34:58 -0700

    Bumped up the parent version of the ACS integration tests POM to 4.7.1-SNAPSHOT and added a new script to run ACS against a local PostgreSQL database with the 'predix' Spring profile

---
+++ service/start-acs-postgres-predix.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#*******************************************************************************
+# Copyright 2016 General Electric Company.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*******************************************************************************
+
+# This script will run ACS against a local PostgreSQL database. To setup the database:
+# 1. Install PostgreSQL
+# 2. Open psql
+# 3. execute: 'create database acs;'
+# 4. execute: 'create user postgres;'
+# 5. execute: 'grant all privileges on database acs to postgres;'
+
+export SPRING_PROFILES_ACTIVE='envDbConfig,predix,simple-cache'
+export DB_DRIVER_CLASS_NAME='org.postgresql.Driver'
+export DB_URL='jdbc:postgresql:acs'
+export DB_USERNAME='postgres'
+export DB_PASSWORD='postgres'
+export DIR=$( dirname "$( python -c "import os; print os.path.abspath('${BASH_SOURCE[0]}')" )" )
+source "${DIR}/start-acs.sh" "$@"
