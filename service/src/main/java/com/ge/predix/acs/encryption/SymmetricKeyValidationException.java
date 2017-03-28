eed5941602e031c6835c820bbbf9e8e0fbab62be
Anubhav <anubhav.saggi@ge.com>
2017-03-27 18:40:35 -0700

    DE25851: Made Encryptor#encrypt thread safe and improved performance by using thread local instances of Cipher
    Signed-off-by: Anurag <anurag.gujral@ge.com>

--- service/src/main/java/com/ge/predix/acs/encryption/SymmetricKeyValidationException.java
+++ service/src/main/java/com/ge/predix/acs/encryption/SymmetricKeyValidationException.java
@@ -3,7 +3,7 @@ package com.ge.predix.acs.encryption;
 @SuppressWarnings("serial")
 public class SymmetricKeyValidationException extends RuntimeException {
     
-    public SymmetricKeyValidationException(final String message) {
-        super(message);
+    public SymmetricKeyValidationException(final String message, final Throwable cause) {
+        super(message, cause);
     }
 }
