7e00b35826479e9e1a3d119d9b0cf9a73a3f79b6
Anubhav <anubhav.saggi@ge.com>
2017-09-14 16:27:55 -0700

    US159762: Updated copyright notices for all source files

--- acs-integration-tests/src/test/java/com/ge/predix/test/utils/PolicyHelper.java
+++ acs-integration-tests/src/test/java/com/ge/predix/test/utils/PolicyHelper.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
- * Copyright 2016 General Electric Company.
+ * Copyright 2017 General Electric Company
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
+
 package com.ge.predix.test.utils;
 
 import static com.ge.predix.test.utils.ACSTestUtil.ACS_VERSION;
--- service/src/test/java/com/ge/predix/controller/test/SubjectPrivilegeManagementControllerIT.java
+++ service/src/test/java/com/ge/predix/controller/test/SubjectPrivilegeManagementControllerIT.java
@@ -1,12 +1,11 @@
-// @formatter:off
 /*******************************************************************************
- * Copyright 2016 General Electric Company.
+ * Copyright 2017 General Electric Company
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +14,7 @@
  * limitations under the License.
  *******************************************************************************/
 
+// @formatter:off
 package com.ge.predix.controller.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
